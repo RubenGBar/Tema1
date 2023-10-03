4c84518feeae8d219c416c54549713fc2e3f817b
rgarcia <rgarcia@106-12.info2>
2023-10-03 12:52:44 +0200

    03/10/2023

--- Parte1/ejercicio1/Ejercicio1.java
+++ Parte1/ejercicio1/Ejercicio1.java
@@ -4,12 +4,16 @@ import java.util.Scanner;
 
 public class Ejercicio1 {
 	public static void main(String[] args) {
+	
+		/* declaración de la variable */
+	int numero;
+	
+	Scanner sc = new Scanner(System.in);
+	
 	/* Le pido que introduzca su nombre */
 	System.out.println("Introduzca un número:");
-	Scanner sc = new Scanner(System.in);
-	/* declaración de la variable */
-	double numero;
-	numero = sc.nextDouble();
+	numero = sc.nextInt();
+	
 	/* Impresión del resultado */
 	System.out.println("Ha escrito " + numero);
 	sc.close();
